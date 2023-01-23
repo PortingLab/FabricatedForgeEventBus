a45f2a31aac8974d00d1835c71fb5cb58951898c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-23 14:22:09 +0800

    clean

--- src/testjars/java/module-info.java
+++
@@ -1,6 +0,0 @@
-open module net.minecraftforge.eventbus.testjars {
-    requires net.minecraftforge.eventbus;
-
-    exports net.minecraftforge.eventbus.testjar;
-    exports net.minecraftforge.eventbus.benchmarks.compiled;
-}
